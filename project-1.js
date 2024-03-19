a0e270a613ddc48a15a59f4f8995df9cfd5d33f4
imperfectcm <imperfect.cm@gmail.com>
2024-03-20 00:06:38 +0800

    second commit

--- project-1.js
+++ project-1.js
@@ -1,1 +1,143 @@
-hello
+const unitLength = 20;
+let boxColor = '#505E71';
+let emptyBoxColor = '#ADBACC';
+let strokeColor = 255;
+let columns; /* To be determined by window width */
+let rows; /* To be determined by window height */
+
+let currentBoard;
+let nextBoard;
+
+
+function setup() {
+    /* Set the canvas to be under the element #canvas*/
+    const canvas = createCanvas(windowWidth - 50, windowHeight - 50);
+    canvas.parent(document.querySelector("#canvas"));
+
+    /*Calculate the number of columns and rows */
+    columns = floor(width / unitLength);
+    rows = floor(height / unitLength);
+
+    /*Making both currentBoard and nextBoard 2-dimensional matrix that has (columns * rows) boxes. */
+    currentBoard = [];
+    nextBoard = [];
+    for (let i = 0; i < columns; i++) {
+        currentBoard[i] = [];
+        nextBoard[i] = [];
+    }
+
+
+    // Now both currentBoard and nextBoard are array of array of undefined values.
+    init(); // Set the initial values of the currentBoard and nextBoard
+}
+
+
+function init() {
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            // let someVariables = <condictions> : <when_true> : <when_false>;
+            // currentBoard[i][j] = random() > 0.8 ? 1 : 0; // one line if
+            currentBoard[i][j] = 0; // one line if
+            nextBoard[i][j] = 0;
+        }
+    }
+
+
+    currentBoard[0][3] = 1
+    currentBoard[1][3] = 1
+    currentBoard[2][3] = 1
+    currentBoard[2][2] = 1
+    currentBoard[1][1] = 1
+
+}
+
+
+function draw() {
+    background(255);
+    generate();
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (currentBoard[i][j] == 1) {
+                fill(boxColor);
+            } else {
+                fill(emptyBoxColor);
+            }
+            stroke(strokeColor);
+            strokeWeight(0.4);
+            rect(i * unitLength, j * unitLength, unitLength, unitLength);
+        }
+    }
+
+}
+
+function generate() {
+    //Loop over every single box on the board
+    for (let x = 0; x < columns; x++) {
+        for (let y = 0; y < rows; y++) {
+            // Count all living members in the Moore neighborhood(8 boxes surrounding)
+            let neighbors = 0;
+            for (let i of [-1, 0, 1]) {
+                for (let j of [-1, 0, 1]) {
+                    if (i == 0 && j == 0) {
+                        // the cell itself is not its own neighbor
+                        continue;
+                    }
+                    // The modulo operator is crucial for wrapping on the edge
+                    neighbors +=
+                        currentBoard[(x + i + columns) % columns][(y + j + rows) % rows];
+                }
+            }
+
+            // Rules of Life
+            if (currentBoard[x][y] == 1 && neighbors < 2) {
+                // Die of Loneliness
+                nextBoard[x][y] = 0;
+            } else if (currentBoard[x][y] == 1 && neighbors > 3) {
+                // Die of Overpopulation
+                nextBoard[x][y] = 0;
+            } else if (currentBoard[x][y] == 0 && neighbors == 3) {
+                // New life due to Reproduction
+                nextBoard[x][y] = 1;
+            } else {
+                // Stasis
+                nextBoard[x][y] = currentBoard[x][y];
+            }
+        }
+    }
+
+    // Swap the nextBoard to be the current Board
+    [currentBoard, nextBoard] = [nextBoard, currentBoard];
+}
+
+
+function mouseDragged() {
+    /**
+     * If the mouse coordinate is outside the board
+     */
+    if (mouseX > unitLength * columns || mouseY > unitLength * rows) {
+        return;
+    }
+
+    const x = Math.floor(mouseX / unitLength);
+
+    const y = Math.floor(mouseY / unitLength);
+    console.log(mouseX, mouseY, x, y)
+    currentBoard[x][y] = 1;
+    fill(boxColor);
+    stroke(strokeColor);
+    rect(x * unitLength, y * unitLength, unitLength, unitLength);
+}
+
+function mousePressed() {
+    noLoop();
+    mouseDragged();
+}
+
+
+function mouseReleased() {
+    loop()
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
